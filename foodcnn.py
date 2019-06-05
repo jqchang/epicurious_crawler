ddcb0c646b2c0cd44dcd26789af4410916b8a88a
Justin Chang <jqchang@gmail.com>
2019-06-05 15:08:33 -0700

    further adjusted parameters for faster learning

--- foodcnn.py
+++ foodcnn.py
@@ -18,13 +18,15 @@ checkpoint = ModelCheckpoint(filepath,
 df = pd.read_csv('food_info_cleaned.csv',header=None)
 datagen=ImageDataGenerator(rescale=1./255,validation_split=0.15)
 train_generator=datagen.flow_from_dataframe(dataframe=df, directory=".",
-            x_col=0, y_col=[1,2,3,4], class_mode="raw", target_size=(413,620), batch_size=32)
+            x_col=0, y_col=[1,2,3,4], class_mode="raw",
+            target_size=(620,413), batch_size=32, subset="training")
 valid_generator=datagen.flow_from_dataframe(dataframe=df, directory=".",
-            x_col=0, y_col=[1,2,3,4], class_mode="raw", target_size=(413,620), batch_size=32)
+            x_col=0, y_col=[1,2,3,4], class_mode="raw",
+            target_size=(620,413), batch_size=32, subset="validation")
 
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=2, padding='same', activation='relu',
-                                input_shape=(413,620,3)))
+                                input_shape=(620,413,3)))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
 model.add(tf.keras.layers.Dropout(0.3))
 
@@ -38,7 +40,7 @@ model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(4, activation='linear'))
 
 model.summary()
-opt = tf.keras.optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+opt = tf.keras.optimizers.Adam(lr=0.3, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 model.compile(loss='mean_squared_error', optimizer=opt)
 STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
 STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
